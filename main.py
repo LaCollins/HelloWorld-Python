ea812f7224cee805962fe15e086efcc746c61d7e
Laura Collins <laura.e.collins1@gmail.com>
2020-10-08 16:49:29 -0500

    done with Python for Beginners Tutorial

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
-weight = input("Weight: ")
-unit = input("(K)g or (L)bs: ")
+# weight = input("Weight: ")
+# unit = input("(K)g or (L)bs: ")
+#
+# if unit.upper() == "K":
+#     weight = float(weight) * 2.20
+#     print("Weight in Lbs:", weight)
+# elif unit.upper() == "L":
+#     weight = float(weight) * 0.45
+#     print("Weight in Kgs:", weight)
 
-if unit.upper() == "K":
-    weight = float(weight) * 2.20462
-    print("Weight in Lbs:", weight)
-elif unit.upper() == "L":
-    weight = float(weight) * 0.453592
-    print("Weight in Kgs:", weight)
+# i = 1
+# while i <= 10:
+#     print(i * '*')
+#     i = i + 1
+
+# names = ['John', 'Bob', 'Mosh', 'Sam', "Mary"]
+# print(names[0:3])
+
+# numbers = [1, 2, 3, 4, 5]
+# for item in numbers:
+#     print(item)
+
+# numbers = range(5, 10, 2)
+# for item in numbers:
+#     print(item)
+#
+# for number in range(5):
+#     print
+
+numbers = (1, 2, 3) #tuple
